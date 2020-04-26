b00dae831e85b258fd63bbdcd9897b895a247232
shehryaarsharif <shehryaarsharif27@gmail.com>
2020-04-26 17:32:29 +0500

    OrderQ DB read functionality Implemented

--- src/components/OrderQ.jsx
+++ src/components/OrderQ.jsx
@@ -29,10 +29,74 @@ function OrderQ() {
 			setData(myData.filter(user=>user.order_id!=idd))
 		}
 		
-		
+		const returnAction=(user)=>{
+			if (user.Action === "Accept/Reject"){
+				return(
+					<td><span className="bg-green pointer grow ba bw1 ma1">Accept</span><span className="bg-red pointer grow ba bw1">Reject</span></td>
+			
+				);
+			}
+			else if (user.Action==="Accept"){
+				return (
+					<td><span className="bg-green">Accepted</span></td>
+				);
+			}
+			else{
+				return(
+					<td><span className="bg-red">Rejected</span></td>
+				);
+			}
+		}
+
+		const returnPrepare=(user)=>{
+
+			if (user.Tracking === "Cancelled"){
+				return(
+					<td><span className="bg-yellow">Cancelled</span></td>
+				);
+			}
+			if (user.Action === "Accept/Reject"){
+				return(
+					<td><p>Waiting for Action</p></td>
+				);
+			}
+			else if(user.Action === "Accept" && user.Tracking === "Pending"){
+				return(
+					<td><span className="bg-gray pointer grow ba bw1 ma1">Prepare</span><span className="bg-light-silver pointer grow ba bw1">Done</span></td>
+				);
+			}
+			else if(user.Tracking === "Preparing"){
+				return(
+					<td><span className="bg-gray 6rem">Preparing</span><span className="bg-light-silver pointer grow ba bw1">Done</span></td>
+	     		);
+			}
+			else return(
+				<td><span>-</span></td>
+			);
+		}
+
+
+		const renderTable = () => {
+	    return myData.map(user => {
+	      return (
+	        <tr>
+	          <td>{user.Date.toDate().getDate()+"-"+(user.Date.toDate().getMonth()+1)+"-"+user.Date.toDate().getFullYear()}</td>
+	          <td>{user.OrderID}</td>
+	          <td>{user.CustomerID}</td>
+	          <td>{user.Address}</td>
+	          <td>{user.DishName.toString()}</td>
+	          <td>{user.DishQuantity.toString()}</td>
+	          <td>{user.Subtotal}</td>
+	          <td>{user.OrderType}</td>
+	          {returnAction(user)}
+	          {returnPrepare(user)}
+	        </tr>
+	      )
+	    })
+	  }
 		
 		return(
-			<div className="tc">
+			<div>
 				<Table responsive>
 				  <thead>
 				    <tr className="bg-light-silver tc ">
@@ -50,12 +114,9 @@ function OrderQ() {
 				  </thead>
 				  <tbody>
 					  {console.log("Doosra",myData)}
-				  	{myData.map(user=> (
-						  
-				  		<tr className="tc">	
-				  			<OrderMngTable data={user} prepared={prepared} />
-				  		</tr>
-				  	))}
+				
+						{renderTable()}
+				  	
 				  </tbody>
 				</Table>
 			</div>
