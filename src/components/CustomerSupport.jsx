249cd39b4c704001d0381df78dbbd0d0d08fe5dd
Mujtaba Ahmed Khawaja <mujtabaahmedkhawaja@hotmail.com>
2020-04-30 17:39:01 +0500

    Customer Support screen made and integrated with the backend. Further changes still need to be done

--- src/components/CustomerSupport.jsx
+++ src/components/CustomerSupport.jsx
@@ -5,10 +5,50 @@ import './CustomerSupport.css'
 import firebase_integration from '../Fire.js';
 
 function CustomerSupport(){
+	const [myData,setData] = useState([]);
+
+	useEffect(()=>{
+		firebase_integration.database.collection('CustomerSupport').onSnapshot((snapshot) => {
+			var order_arr = []
+			snapshot.docs.forEach(doc => {
+				order_arr.push(doc.data())
+			});
+			setData(order_arr)
+			console.log(order_arr)
+		})
+	}, myData);
+
+	const renderTable = () => {
+	    return myData.map(feedback => {
+	      return (
+	        <tr>
+	          <td>{feedback.CustomerID}</td>
+			  <td>{feedback.Date.toDate().getDate()+"-"+(feedback.Date.toDate().getMonth()+1)+"-"+feedback.Date.toDate().getFullYear()}</td>
+	          <td>{feedback.Rating}</td>
+	          <td>{feedback.Subject}</td>
+	          <td>{feedback.Message}</td>
+	        </tr>
+	      )
+	    })
+	  }
+
 	return(
 		<div>
-			<img className="text-example" src={pic}/>
-			
+			<div>
+				<img className="text-example" src={pic}/>
+			</div>
+			<Table responsive>
+			<thead>
+				<tr className="bg-light-silver">
+					<th>CUSTOMER ID</th>
+					<th>DATE</th>
+					<th>RATING</th>
+					<th>SUBJECT</th>
+					<th>MESSAGE</th> 	
+				</tr>
+			</thead>
+			<tbody>{renderTable()}</tbody>
+			</Table>	
 		</div>
 	);
 
