6d7f1ba86cb7564e85a276e0a8c6aa12bd6b5dca
Omer <omeriqbal7@hotmail.com>
2020-05-01 05:51:01 +0500

    checkbox button added

--- src/components/AdminMenu.jsx
+++ src/components/AdminMenu.jsx
@@ -1,5 +1,6 @@
 import React , { useState, useEffect } from 'react';
 import './AdminMenu.css';
+// import EdiText from 'react-editext'
 
 function AdminMenu(){
 	const [menu, setmenu] = useState([])
@@ -46,6 +47,7 @@ function AdminMenu(){
 						<table id="items" className="table table-hover">
 							<thead>
 								<tr>
+								<th/>
 								<th style = {{color: ""}} scope="col">ID</th>
 								<th style = {{color: "3C3C3C"}} scope="col">Name</th>
 								<th style = {{color: "3C3C3C"}} scope="col">Category</th>
@@ -64,6 +66,7 @@ function AdminMenu(){
 										(x, i) => {
 											return (
 												<tr key = {x.ID}>
+													<td><input type="checkbox" class="form-check-input"/></td>
 													<td style = {{color: "3C3C3C"}}>{data[i].ID}</td>
 													<td style = {{color: "3C3C3C"}}>{data[i].Name}</td>
 													<td style = {{color: "3C3C3C"}}>{data[i].Category}</td>
@@ -71,6 +74,7 @@ function AdminMenu(){
 													<td style = {{color: "3C3C3C"}}>{data[i].Description}</td>
 													<td style = {{color: "3C3C3C"}}>{data[i].PortionSize}</td>
 													<td style = {{color: "#576271"}}>{data[i].PrepTime}</td>
+													{/* <td style = {{color: "#576271"}}><textarea ref="newText" defaultValue="Edit me"/></td> */}
 													<td><button type="button" class="btn btn-primary btn-sm imagebutton">Upload Image</button></td>
 												</tr>
 											);
