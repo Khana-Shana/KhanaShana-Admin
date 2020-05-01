40b8de5f226c2d1d500d242c1490433a5b91a0a7
Omer <omeriqbal7@hotmail.com>
2020-05-02 02:04:43 +0500

    fixing placeholder values

--- src/components/AdminMenu.jsx
+++ src/components/AdminMenu.jsx
@@ -4,42 +4,48 @@ import firebase_integration from '../Fire.js'
 
 function AdminMenu(){
 	const [menu, setmenu] = useState([])
-	const [edit, setedit] = useState([])
+	// const [edit, setedit] = useState([])
 	const [editmode, seteditmode] = useState(false)
+	const [filteredmenu, setfilteredmenu] = useState([])
 	useEffect(() => {
 		firebase_integration.database.collection('Menu').onSnapshot((snapshot) => {
 			var menu_items = []
-			// var edit_status = []
 					snapshot.docs.forEach(doc => {
 					menu_items.push(doc.data())
-					// edit_status.push(false)
 				});
 					setmenu(menu_items)
-					// setedit(edit_status)
+					setfilteredmenu(menu_items)
 				})
 
 	}, menu)
 	function handling_editmode() {
 		var editstates = [];
-		[...menu.keys()].map((_,i) => {
+		var newmenu = []
+		menu.map((_,i) => {
 			editstates.push(document.getElementsByClassName("form-check-input")[i].checked)
-			// edit[i] === true
-			// 	?editstates[i].push(true)
-			// 	:editstates.push(document.getElementsByClassName("form-check-input")[i].checked)
+			if(document.getElementsByClassName("form-check-input")[i].checked)
+			{
+				newmenu.push(menu[i])
+			}
 		})
-		setedit(editstates)
+		setfilteredmenu(newmenu)
 		seteditmode(true)
 	}
 	return(
 		<div>
+			{/* {console.log(filteredmenu)} */}
+			{/* <input type="text" placeholder="Name"/> */}
 			<div id="menubox" className="container">
 				<div className="row">
 					<button type="button" class="btn btn-primary btn-sm menubutton">Add</button>
-					<button id = "edit" type="button" class="btn btn-primary btn-sm menubutton" onClick = {() => handling_editmode()}>Edit</button>
 					<button type="button" class="btn btn-primary btn-sm menubutton">Remove</button>
+					{editmode
+						?<button id = "edit" type="button" class="btn btn-primary btn-sm menubutton">Save</button>
+						:<button id = "edit" type="button" class="btn btn-primary btn-sm menubutton" onClick = {() => handling_editmode()}>Edit</button>
+					}
 				</div>
 				<div className="row">
-					<div class="table-responsive">
+					<div className="table-responsive">
 						<table id="items" className="table table-hover">
 							<thead>
 								{editmode === true?
@@ -66,32 +72,32 @@ function AdminMenu(){
 								}
 							</thead>
 							{
-								[...menu.keys()].map(
+								[...filteredmenu.keys()].map(
 									(x, i) => {
 										return (
 											<tbody>
-												{editmode === true && edit[i] === true?
-												<tr key = {x.ID}>
-													<td style = {{color: "3C3C3C"}}>{menu[i].DishID}</td>
-													<td><input class="form-control form-control-sm" type="text" placeholder="Name"/></td>
-													<td><input class="form-control form-control-sm" type="text" placeholder="Category"/></td>
-													<td><input class="form-control form-control-sm" type="text" placeholder="Price"/></td>
-													<td><input class="form-control form-control-sm" type="text" placeholder="Description"/></td>
-													<td><input class="form-control form-control-sm" type="text" placeholder="Portion Size"/></td>
-													<td><input class="form-control form-control-sm" type="text" placeholder="Prep Time"/></td>
-													<td><button type="button" class="btn btn-primary btn-sm imagebutton">Upload Image<input type="file"/></button></td>
-												</tr>:
-												<tr key = {x.ID}>
-													<td><input type="checkbox" class="form-check-input"/></td>
-													<td style = {{color: "3C3C3C"}}>{menu[i].DishID}</td>
-													<td style = {{color: "3C3C3C"}}>{menu[i].Name}</td>
-													<td style = {{color: "3C3C3C"}}>{menu[i].Category}</td>
-													<td style = {{color: "3C3C3C"}}>{menu[i].SalePrice}</td>
-													<td style = {{color: "3C3C3C"}}>{menu[i].Description}</td>
-													<td style = {{color: "3C3C3C"}}>{menu[i].PortionSize}</td>
-													<td style = {{color: "#576271"}}>{menu[i].PrepTime}</td>
-												</tr>														
-											}
+												{editmode === true
+													?<tr key = {x.ID}>
+														<td style = {{color: "3C3C3C"}}>{menu[i].DishID}</td>
+														<td><input class="form-control form-control-sm" type="text" placeholder="Name" value/></td>
+														<td><input class="form-control form-control-sm" type="text" placeholder="Category"/></td>
+														<td><input class="form-control form-control-sm" type="text" placeholder="Price"/></td>
+														<td><input class="form-control form-control-sm" type="text" placeholder="Description"/></td>
+														<td><input class="form-control form-control-sm" type="text" placeholder="Portion Size"/></td>
+														<td><input class="form-control form-control-sm" type="text" placeholder="Prep Time"/></td>
+														<td><button type="button" class="btn btn-primary btn-sm imagebutton">Upload Image<input type="file"/></button></td>
+													</tr>
+													:<tr key = {x.ID}>
+														<td><input type="checkbox" class="form-check-input"/></td>
+														<td style = {{color: "3C3C3C"}}>{filteredmenu[i].DishID}</td>
+														<td style = {{color: "3C3C3C"}}>{filteredmenu[i].Name}</td>
+														<td style = {{color: "3C3C3C"}}>{filteredmenu[i].Category}</td>
+														<td style = {{color: "3C3C3C"}}>{filteredmenu[i].SalePrice}</td>
+														<td style = {{color: "3C3C3C"}}>{filteredmenu[i].Description}</td>
+														<td style = {{color: "3C3C3C"}}>{filteredmenu[i].PortionSize}</td>
+														<td style = {{color: "#576271"}}>{filteredmenu[i].PrepTime}</td>
+													</tr>														
+												}
 											</tbody>
 										);
 									}
