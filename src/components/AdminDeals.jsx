1242e16171e51954ad42eb4f4a14bbcc3e4ffd9a
Omer <omeriqbal7@hotmail.com>
2020-05-05 07:05:48 +0500

    discount button added

--- src/components/AdminDeals.jsx
+++ src/components/AdminDeals.jsx
@@ -8,6 +8,11 @@ function AdminDeals() {
         <div>
             <div className = "container mainbox">
                 <div className = "row">
+                    <div className = "col d-flex justify-content-end" style = {{marginBottom: "1%"}}>
+                        <button type="button" className="btn btn-primary btn-sm dealbutton">Discount >></button>
+                    </div>
+                </div>
+                <div className = "row">
                     <div className = "col">
                         <DealCard
                             dealtype = "Daily Deal"
