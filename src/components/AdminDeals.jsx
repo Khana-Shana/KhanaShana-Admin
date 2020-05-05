02bf7f81b13d3b82b00e045e544fa9a13add007b
Mujtaba Ahmed Khawaja <mujtabaahmedkhawaja@hotmail.com>
2020-05-05 08:35:23 +0500

    Updated deals with bk

--- src/components/AdminDeals.jsx
+++ src/components/AdminDeals.jsx
@@ -4,13 +4,81 @@ import DealCard from './DealCard';
 import firebase_integration from '../Fire.js'
 
 function AdminDeals() {
-    const [dailydeal, setdailydeal] = React.useState()
-    const [weeklydeal, setweeklydeal] = React.useState()
+    const [dailydeal, setdailydeal] = React.useState("")
+    const [weeklydeal, setweeklydeal] = React.useState("")
     React.useEffect( ()=> {
-        firebase_integration.collection("Deals").onSnapshot((snapshot) => {
-            
+        var deals = []
+        firebase_integration.database.collection("Deals").orderBy("DealType", "desc").onSnapshot((snapshot) => {
+            snapshot.forEach((doc) => {
+                console.log(doc.data())
+                deals.push(doc.data())
+            })
+            if(deals.length === 2){
+                setweeklydeal(deals[1])
+                setdailydeal(deals[0])  
+            }
+            else if(deals.length === 1){
+                if(deals[0].DealType === "Daily") {
+                    setdailydeal(deals[0])
+                }
+                else {
+                    setweeklydeal(deals[0])
+                }
+            }
         })
-    })
+    }, [])
+    
+    // function addDailyDeal() {
+    //     firebase_integration.collection("Deals").doc("Daily").set({
+    //         DealType: "Daily",
+    //         Name: ,
+    //         Price: ,
+    //         MenuID: ,
+    //         ImageName: ,
+    //         ImageURL: ""
+    //     })
+    // }
+
+    // function addWeeklyDeal() {
+    //     firebase_integration.collection("Deals").doc("Weekly").set({
+    //         DealType: "Weekly",
+    //         Name: ,
+    //         Price: ,
+    //         MenuID: ,
+    //         ImageName: ,
+    //         ImageURL: ""
+    //     })
+    // }
+
+    // function uploadDealImage(id, dealtype){
+	// 	var image = document.getElementById(id).files[0]
+	// 	var imageName = image.name
+	// 	console.log(imageName)
+	// 	var uploadTask = firebase_integration.storage.ref().child('Deals/'+imageName).put(image);
+	// 	uploadTask.on('state_changed', 
+	// 	function(snapshot) {
+	// 		var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+	// 		console.log('Upload is ' + progress + '% done');
+	// 	}, function(error) {
+	// 		alert(error.message)
+	// 	}, function() {
+	// 		firebase_integration.storage.ref().child('Deals/'+imageName).getDownloadURL().then(function(downloadURL) {
+	// 		firebase_integration.database.collection('Deals').doc(dealtype()).update({
+	// 				ImageName: imageName,
+	// 				ImageURL: downloadURL
+	// 			})
+	// 		});
+	// 	});
+    // }
+    
+    // function removeDeal(dealtype){
+    //     async function deleteImage(){
+    //         var docs = await firebase_integration.database.collection('Deals').doc(dealtype).get()
+    //         firebase_integration.storage.ref().child('Deals/'+docs.data().ImageName).delete()
+    //     }
+    //     firebase_integration.database.collection("Deals").doc(dealtype).delete()
+    // }
+
     return (
         <div>
             <div className = "container mainbox">
