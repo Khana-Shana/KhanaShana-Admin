af0a3efe37858d295553bbb6097d3049d742668b
Mujtaba Ahmed Khawaja <mujtabaahmedkhawaja@hotmail.com>
2020-05-05 07:38:27 +0500

    bla bla

--- src/components/AdminDB.jsx
+++ src/components/AdminDB.jsx
@@ -12,7 +12,6 @@ function AdminDB(){
 					<AdminDBTable/> 
 				</div>
 			</div>
-
 			<div className="col-md-6 no-gutters">
 				<div className="rightside d-flex justify-content-center align-items center">
 					<img id="aunty" src = "https://firebasestorage.googleapis.com/v0/b/khana-shana-2020.appspot.com/o/AdminDatabase%2FGroup%2011831.svg?alt=media&token=7ae930c8-05ec-4c8a-983a-5fa4916c859b"/>
