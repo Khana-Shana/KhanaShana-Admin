7418f21246d953a2ac5c7eb6e5bcecfd5ba4426e
Mujtaba Ahmed Khawaja <mujtabaahmedkhawaja@hotmail.com>
2020-05-06 07:41:32 +0500

    Discount wheel bk in progress

--- src/components/DiscountWheel.jsx
+++ src/components/DiscountWheel.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import './DiscountWheel.css';
+import firebase_integration from '../Fire.js'
 
 function DiscountWheel() {
-    
+    const [mydata1, setdata1] = React.useState(0)
+    const [mydata2, setdata2] = React.useState(0)
+    const [mydata3, setdata3] = React.useState(0)
+
+    React.useEffect(() => {
+        firebase_integration.database.collection("DiscountWheel").onSnapshot((snapshot) => {
+            var data = []
+            snapshot.forEach((doc) => {
+                data.push(doc.data().value.split("%")[0])
+            })
+            setdata1(data[0])
+            setdata2(data[1])
+            setdata3(data[2])
+        })
+    },[])
+
+    function updateDB(){
+        firebase_integration.database.collection("DiscountWheel").doc("1").set({
+            value: mydata1+"%"
+        })
+        firebase_integration.database.collection("DiscountWheel").doc("2").set({
+            value: mydata2+"%"
+        })
+        firebase_integration.database.collection("DiscountWheel").doc("3").set({
+            value: mydata3+"%"
+        })
+    }
+
     return (
         <div>
             
@@ -19,24 +47,24 @@ function DiscountWheel() {
                              <div className="form-group row">
                                 <label className="col-lg-2 col-form-label">Discount 1</label>
                                 <div className="col-lg-10">
-                                    <input type="text" className="form-control form-control-sm" placeholder="Enter Discount Percentage" ></input>
+                                    <input type="number" className="form-control form-control-sm" placeholder="Enter Discount Percentage" maxLength="2" min="0" max="99" value={mydata1} onChange={(e)=>{setdata1(e.target.value)}}></input>
                                 </div>
                             </div>
                             <div className="form-group row">
                                 <label className="col-lg-2 col-form-label">Discount 2</label>
                                 <div className="col-lg-10">
-                                    <input type="text" className="form-control form-control-sm" placeholder="Enter Discount Percentage" ></input>
+                                    <input type="number" className="form-control form-control-sm" placeholder="Enter Discount Percentage" maxLength="2" min="0" max="99" value={mydata2} onChange={(e)=>{setdata2(e.target.value)}}></input>
                                 </div>
                             </div>
                             <div className="form-group row">
                                 <label className="col-lg-2 col-form-label">Discount 3</label>
                                 <div className="col-lg-10">
-                                    <input type="text" className="form-control form-control-sm" placeholder="Enter Discount Percentage" ></input>
+                                    <input type="number" className="form-control form-control-sm" placeholder="Enter Discount Percentage" maxLength="2" min="0" max="99" value={mydata3} onChange={(e)=>{setdata3(e.target.value)}}></input>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className = "col d-flex justify-content-center">
-                                    <button type="button" className="btn btn-primary btn-sm updatebutton">Update Discount Wheel</button>
+                                    <button type="button" className="btn btn-primary btn-sm updatebutton" onClick={()=>updateDB()}>Update Discount Wheel</button>
                                 </div>
                             </div>
                         </form>
