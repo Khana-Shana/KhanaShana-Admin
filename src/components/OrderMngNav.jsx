1cb8aae1dad33a78be94129857a3a7b3213dbf03
shehryaarsharif <shehryaarsharif27@gmail.com>
2020-05-06 20:43:17 +0500

    OrderQ css done

--- src/components/OrderMngNav.jsx
+++ src/components/OrderMngNav.jsx
@@ -9,9 +9,9 @@ function OrderMngNav(props) {
 		return(
 			<div>
 				<div className="btn-group pa3 w-50">
-					<Link to="/order" type="button" className={props.colr==0?"btn w-50 white customColr":"btn w-50 black"}>Queue</Link>
-					<Link to="/completedOrders" type="button" className={props.colr==1?"btn w-50 white customColr":"btn w-50 black"}>Completed Orders</Link>
-					<Link to="/history" type="button" className={props.colr==2?"btn w-50 white customColr":"btn w-50 black"}>History</Link>
+					<Link to="/order" type="button" className="btn w-50" style={{backgroundColor: props.colr==0?"#955F61":"#5c5a5a"}}>Queue</Link>
+					<Link to="/completedOrders" type="button" className="btn w-50" style={{backgroundColor: props.colr==1?"#955F61":"#5c5a5a"}}>Completed Orders</Link>
+					<Link to="/history" type="button" className="btn w-50" style={{backgroundColor: props.colr==2?"#955F61":"#5c5a5a"}}>History</Link>
 				</div>   
 			</div>
 		);		
