39cd11d92b7f6299a9bbfc92f63b5594125ac9e2
shehryaarsharif <shehryaarsharif27@gmail.com>
2020-05-06 18:14:57 +0500

    Link instead of <a>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Card.css";	
+import {Link} from "react-router-dom";
 	
 
 const Card = () => {
@@ -8,14 +9,14 @@ const Card = () => {
 			
 			<div className="grid-layout mt5"> {/* grid-layout css taken from https://stackoverflow.com/questions/8470070/how-to-create-grid-tile-view*/}
 				
-				<a type="button" href="/order" className="item grow span-3"><h1 className="mera">Order Management</h1></a>				
-				<a type="button" href="/adminMenu" className="item grow span-3 pa3" ><h1>Menu</h1></a>	
-				<a type="button" href="/deals" className="item grow span-3"><h1 className="pa4.5">Deals & Discounts</h1></a>
-				<a type="button" href="/customerSupport" className="item grow span-3"><h1 >Customer Support</h1></a>
-				<a type="button" href="/adminDB" className="item grow span-3"><h1>Admin Database</h1></a>
-				<a type="button" href="/customerDB" className="item grow span-3"><h1>Customer Database</h1></a>
-				<a type="button" href="/createAccount" className="item grow span-3"><h1 >Create Account</h1></a>
-				<a type="button" href="/restDetails" className="item grow span-3"><h1 >Restaurant Details</h1></a>
+				<Link to="/order" type="button" className="item grow span-3"><h1 >Order Management</h1></Link>			
+				<Link to="/adminMenu" type="button" className="item grow span-3 pa3" ><h1>Menu</h1></Link>	
+				<Link to="/deals" type="button" className="item grow span-3"><h1>Deals & Discounts</h1></Link>
+				<Link to="/customerSupport" type="button" className="item grow span-3"><h1 >Customer Support</h1></Link>
+				<Link to="/adminDB" type="button" className="item grow span-3"><h1>Admin Database</h1></Link>
+				<Link to="/customerDB" type="button" className="item grow span-3"><h1>Customer Database</h1></Link>
+				<Link to="/createAccount" type="button" className="item grow span-3"><h1 >Create Account</h1></Link>
+				<Link to="/order" type="button" className="item grow span-3"><h1 >Restaurant Details</h1></Link>
 
 			</div>
 		 	   
